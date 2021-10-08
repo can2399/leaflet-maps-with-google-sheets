f032fa94663037d587e0d6b92591d699ada4cd8f
can2399 <32220576+can2399@users.noreply.github.com>
2021-10-08 05:30:25 +0300

    Update google-doc-url.js
    E-tablo linki değiştirildi.

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,7 @@
 // paste in your published Google Sheets URL from the browser address bar
 var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/1dpAfN0I_XjQQqjlIowrSh3MFZqgEmTVnuwLPZ_1aC9g/edit?usp=sharing';
+
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+// var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
